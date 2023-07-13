3ecd76d4ff929ede3699e5c0eacfaa340def1516
Daniela Huppenkothen <d.huppenkothen@sron.nl>
2023-07-13 11:02:40 +0200

    Added top-level imports

--- spexai/__init__.py
+++ spexai/__init__.py
@@ -8,3 +8,9 @@ __version__ = "0.0.1"
 __author__ = 'Jip Matthijsse'
 __credits__ = 'SRON / Universiteit van Amsterdam'
 
+
+# import inference code at top-level for easier use
+from spexai.inference.model import *
+from spexai.inference.fit import *
+from spexai.inference.write_tensors import *
+
